b94656c4a59368357407820c5b70707a3cba8e93
Kara <palumbokara@yahoo.com>
2021-08-12 17:02:10 -0600

    fixed seed file

--- backend/petfinder-seed.sql
+++ backend/petfinder-seed.sql
@@ -15,10 +15,10 @@ VALUES
     (51012838, 'Violet', 'Domestic Short Hair', 'Adult', 'Female', 'Gray', 'Hi, My name is Violet! I am a 2 year old, front declawed female with LOTS of love to give', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012838\/1\/?bust=1616784629\u0026width=300'),
     (51012585, 'Bebe', 'Cairn Terrier', 'Adult', 'Female', 'Chocolate', 'Bebe came to us at Forever Loved when her owner passed away.', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012585\/1\/?bust=1616784394\u0026width=100'),
     (51012482, 'Tinkerbell', 'Domestic Short Hair', 'Young', 'Female', 'Torbie', 'One -year-old Tinkerbell is about to add some fairy dust to your life! She is a gregarious and friendly lady', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012482\/1\/?bust=1616783821\u0026width=100'),
-    (51012857, 'Fleetwood Mac', 'Husky', 'Adult', 'Female', 'Black', 'Hello I am Fleetwood Mac!', ''),
+    (51012857, 'Fleetwood Mac', 'Husky', 'Adult', 'Female', 'Black', 'Hello I am Fleetwood Mac!', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'),
     (51012858, 'Zander', 'Pit Bull Terrier', 'Adult', 'Male', 'Black', 'Fun loving Zander waiting for you to take him home!', ''),
-    (51012855, 'Tank', 'Pit Bull Terrier', 'Adult', 'Male', 'Yellow', 'Hi! My name is Tank and I just arrived at Santa Maria Campus. I am a 4-year-old, Male, American Pit Bull Terrier who loves to play.', ''),
-    (51012856, 'Uma', 'Pit Bull Terrier', 'Adult', 'Female', 'Yellow', 'Hi! My name is Uma and I just arrived at Santa Maria Campus. I am a 2-year-old, female, American Pit Terrier who loves to cuddle.', '');
+    (51012855, 'Tank', 'Pit Bull Terrier', 'Adult', 'Male', 'Yellow', 'Hi! My name is Tank and I just arrived at Santa Maria Campus. I am a 4-year-old, Male, American Pit Bull Terrier who loves to play.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'),
+    (51012856, 'Uma', 'Pit Bull Terrier', 'Adult', 'Female', 'Yellow', 'Hi! My name is Uma and I just arrived at Santa Maria Campus. I am a 2-year-old, female, American Pit Terrier who loves to cuddle.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif');
 
 INSERT INTO favorites (username, pet_id)
 VALUES
