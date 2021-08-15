d23d6fbb54e6ee367820b47e139d20f1bd8126d1
Kara <palumbokara@yahoo.com>
2021-08-15 12:38:55 -0600

    seeded more pets

--- backend/petfinder-seed.sql
+++ backend/petfinder-seed.sql
@@ -9,16 +9,28 @@ VALUES ('testuser',
 
 INSERT INTO pets (pet_id, name, species, age, gender, color, description, photos)
 VALUES
-    (51012551, 'Rudy', 'Guinea Pig', 'Adult', 'Male', 'Brown', 'My name is Rudy! The humans at the shelter tell me that Im one of the coolest guinea pigs they have ever met.', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012551\/1\/?bust=1616784128\u0026width=100'),
-    (51012574, 'Lovebug', 'Domestic Short Hair', 'Senior', 'Female', 'Gray', 'Meow. Have you ever seen a kitty as adorable as me?!', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012574\/2\/?bust=1616784281\u0026width=100'),
-    (51012833, 'Izzy', 'Terrier', 'Adult', 'Female', 'Cream', 'Izzy. 4 Years Old. Spayed Female. Terrier Mix. 36 lbs. Since you are reading this, you are obviously looking for me!', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012833\/1\/?bust=1616784627\u0026width=100'),
-    (51012838, 'Violet', 'Domestic Short Hair', 'Adult', 'Female', 'Gray', 'Hi, My name is Violet! I am a 2 year old, front declawed female with LOTS of love to give', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012838\/1\/?bust=1616784629\u0026width=300'),
-    (51012585, 'Bebe', 'Cairn Terrier', 'Adult', 'Female', 'Chocolate', 'Bebe came to us at Forever Loved when her owner passed away.', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012585\/1\/?bust=1616784394\u0026width=100'),
-    (51012482, 'Tinkerbell', 'Domestic Short Hair', 'Young', 'Female', 'Torbie', 'One -year-old Tinkerbell is about to add some fairy dust to your life! She is a gregarious and friendly lady', 'https:\/\/dl5zpyw5k3jeb.cloudfront.net\/photos\/pets\/51012482\/1\/?bust=1616783821\u0026width=100'),
+    (51012551, 'Rudy', 'Guinea Pig', 'Adult', 'Male', 'Brown', 'My name is Rudy! The humans at the shelter tell me that Im one of the coolest guinea pigs they have ever met.', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/51012551/1/?bust=1616784128u0026'),
+    (51012574, 'Lovebug', 'Domestic Short Hair', 'Senior', 'Female', 'Gray', 'Meow. Have you ever seen a kitty as adorable as me?!', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/51012574/2/?bust=1616784281u0026'),
+    (51012833, 'Izzy', 'Terrier', 'Adult', 'Female', 'Cream', 'Izzy. 4 Years Old. Spayed Female. Terrier Mix. 36 lbs. Since you are reading this, you are obviously looking for me!', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/51012833/1/?bust=1616784627u0026'),
+    (51012838, 'Violet', 'Domestic Short Hair', 'Adult', 'Female', 'Gray', 'Hi, My name is Violet! I am a 2 year old, front declawed female with LOTS of love to give', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/51012838/1/?bust=1616784629u0026'),
+    (51012585, 'Bebe', 'Cairn Terrier', 'Adult', 'Female', 'Chocolate', 'Bebe came to us at Forever Loved when her owner passed away.', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/51012585/1/?bust=1616784394u0026'),
+    (51012482, 'Tinkerbell', 'Domestic Short Hair', 'Young', 'Female', 'Torbie', 'One -year-old Tinkerbell is about to add some fairy dust to your life! She is a gregarious and friendly lady', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/51012482/1/?bust=1616783821u0026'),
     (51012857, 'Fleetwood Mac', 'Husky', 'Adult', 'Female', 'Black', 'Hello I am Fleetwood Mac!', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'),
-    (51012858, 'Zander', 'Pit Bull Terrier', 'Adult', 'Male', 'Black', 'Fun loving Zander waiting for you to take him home!', ''),
+    (51012858, 'Zander', 'Pit Bull Terrier', 'Adult', 'Male', 'Black', 'Fun loving Zander waiting for you to take him home!', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'),
     (51012855, 'Tank', 'Pit Bull Terrier', 'Adult', 'Male', 'Yellow', 'Hi! My name is Tank and I just arrived at Santa Maria Campus. I am a 4-year-old, Male, American Pit Bull Terrier who loves to play.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'),
-    (51012856, 'Uma', 'Pit Bull Terrier', 'Adult', 'Female', 'Yellow', 'Hi! My name is Uma and I just arrived at Santa Maria Campus. I am a 2-year-old, female, American Pit Terrier who loves to cuddle.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif');
+    (51012856, 'Uma', 'Pit Bull Terrier', 'Adult', 'Female', 'Yellow', 'Hi! My name is Uma and I just arrived at Santa Maria Campus. I am a 2-year-old, female, American Pit Terrier who loves to cuddle.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'), 
+    (52673930, 'Marge', 'Rabbit', 'Young', 'Female', 'Brown', 'Marge and Home are siblings and they come together to bring you a life filled with cuddles', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52673930/1/?bust=1629050168&'), 
+    (52673927, 'Latte', 'Domestic Short Hair', 'Baby', 'Female', 'Tortoiseshell', 'This tiny but fierce kitty is the complete package. She is beautiful, playful and loving', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52673927/1/?bust=1629050146&'), 
+    (52673928, 'Ace', 'Pit Bull Terrier', 'Baby', 'Male', 'Tricolored', 'Ace is a mild mannered, well-behaved puppy who was found as a stray and we guesstimate him to be about 1 years old', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'), 
+    (52673929, 'Gracie', 'Domestic Short Hair', 'Baby', 'Female', 'Gray', 'Gracie may be a teeny girl, but her personality is spunky!', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'),
+    (52673922, 'Penny', 'Chihuahua', 'Young', 'Female', 'Cream', 'Penny loves to play and even more so enjoys treats after her walks.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'), 
+    (52673923, 'Luna', 'Dachshund', 'Baby', 'Female', 'Black', 'Hi my name is Luna and I love going on walks and playing with toys.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'), 
+    (52674068, 'Judge', 'American Staffordshire Terrier', 'Adult', 'Male', 'Brown and white', 'Judge is a senior boy who is complete blind. He came to us from a shelter in Southern WVa and is looking for a home with a loving family.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif'), 
+    (52674066, 'Kai', 'Domestic Short Hair', 'Baby', 'Male', 'Tabby', 'Meet baby Kai! He is a 13 week old, 3  pound male and is ready to be adopted!', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52674066/1/?bust=1629051099&'), 
+    (52674065, 'Opal', 'Domestic Short Hair', 'Adult', 'Female', 'Dilute Calico', 'Opal, the sweetest cuddler you will ever meet. She is available for adoption!', 'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/52674065/1/?bust=1629051098&'),
+    (52674233, 'Elektra', 'Border Collie', 'Baby', 'Female', 'White and brown', 'Elektra is a sweet, petite girl who is a bit shy at first but warms up after a few cuddles.', 'https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif');
+
+
 
 INSERT INTO favorites (username, pet_id)
 VALUES
